b1c7b9d6e20c86d2a4d9d41da73116f5894e5d89
Nele Quast <nele.quast@dtc.ox.ac.uk>
2021-10-18 16:00:34 +0100

    refactor setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,11 @@ from setuptools import setup
 
 setup(
     name='ci_demo',
-
     version='0.1',
-
     description='Demonstration of setting up continuous integration for a Python project',
-
     license='MIT',
-
     packages=['ci_course'],
-
-    install_requires=[
-    ],
-
+    install_requires=[],
     extras_require={
         'docs': [
             'sphinx'
@@ -22,6 +15,5 @@ setup(
             'flake8',
         ],
     },
-
     zip_safe=False
 )
